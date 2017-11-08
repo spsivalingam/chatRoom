2aa3272089bfb7d92541d24be4a733616f3b3bb5
spsivalingam <ssubbiah@uncc.edu>
2017-11-08 12:32:55 -0500

    chatRoomActivity

---
+++ Chat_Room/app/src/main/java/uncc/sivalingam/chat_room/ChatRoomActivity.java
@@ -0,0 +1,16 @@
+package uncc.sivalingam.chat_room;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChatRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room);
+        setTitle("Chatroom");
+
+
+    }
+}
